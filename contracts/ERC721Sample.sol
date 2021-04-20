d31411fbf609ba6309fc3da79a3da98bc46fe36a
sahin.hakan <hakansahince@gmail.com>
2021-04-20 22:02:29 +0300

    lockToken ve unlockToken fonksiyonları yazıldı

--- contracts/ERC721Sample.sol
+++ contracts/ERC721Sample.sol
@@ -9,23 +9,42 @@ contract Bridge721 is ERC721 {
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    //mapping(string => uint8) hashes;
-    mapping(uint256 => address) public _tokenOwners;
-    mapping(address => uint256) public _tokens;
+    //mapping(uint256 => address) public _tokenOwners; ERC721'in standartında var zaten bu alan. ownerOf metodu ile de cevabı alabiliyorum
+    //mapping(address => uint256) public _tokens;
+    uint256 zeroPointOneAvax = (10 ** 17);
+    address payable contratAddress;
+    mapping(uint256 => address) private originalOwner;
 
     constructor() ERC721("Bridge721", "bNFT") {
+        contratAddress = payable(msg.sender);
     }
 
-    function _mint721() public payable{
+    function mint721() public payable{
         uint256 id = _tokenIds.current();
-        _tokenOwners[id] = msg.sender;
-        _tokens[msg.sender] = id;
+        //_tokenOwners[id] = msg.sender;
+        //_tokens[msg.sender] = id;
         _mint(msg.sender, id);
         _tokenIds.increment();
     }
     
-    function getTokens() public view returns (uint256){
-        return _tokens[msg.sender];
+    // function getTokens() public view returns (uint256){
+    //     return _tokens[msg.sender];
+    // }
+
+    //ownerOf(uint256 tokenId) owner'ı buradan buluruz aslında
+
+    function lockToken(uint256 tokenId) public payable {
+        //şimdilik işlem ücreti olmasın
+        //require(msg.value >= zeroPointOneAvax, "Ether value is below the lock price");
+
+        transferFrom(msg.sender, contratAddress, tokenId);
+        originalOwner[tokenId] = msg.sender;
+        _approve(msg.sender, tokenId);//lock larken approve yetkisini kişiye vermek lazım yoksa daha sonrasında yapılamıyor.
+    }
+    
+    function unlockToken(uint256 tokenId) public payable {
+        require(msg.sender == originalOwner[tokenId], "This NFT is not yours");
+        transferFrom(contratAddress, msg.sender, tokenId);
     }
     
 }
